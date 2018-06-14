d08c2d56a2ebf937a1910fe6904d74030cd5b53f
Sahil Mepani <productreseller@gmail.com>
2018-06-15 00:09:29 +0530

    Comment grunt jquery ready package

--- source/Gruntfile.js
+++ source/Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
-	grunt.loadNpmTasks( 'grunt-jquery-ready' );
+	// grunt.loadNpmTasks( 'grunt-jquery-ready' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 
 	grunt.registerTask( 'default', [ 'sass', 'concat', 'watch' ] );

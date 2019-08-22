5d957277fe9a678023d4399f92045c4eb81673cb
Sahil <productreseller@gmail.com>
2019-08-22 14:55:01 +0530

    Add U/A parser script to detect devices and browsers

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
+/*=================================
+=            UA Parser            =
+=================================*/
+// https://github.com/faisalman/ua-parser-js
+var parser = new UAParser();
+var result = parser.getResult();
+console.log(result.browser.name);
+console.log(parseInt(result.browser.version.split('.')[0], 10));
+console.log(result.device.type);
+console.log(result.os.name);
+console.log(result.os.version);
+
+
 jQuery( document ).ready( function( $ ) {;
 $( '.header-nav-toggle' ).click( function( e ) {
 	$( this ).toggleClass( 'js-active' );
--- source/js/custom/!document.ready.js
+++ source/js/custom/!document.ready.js
@@ -1,1 +1,14 @@
+/*=================================
+=            UA Parser            =
+=================================*/
+// https://github.com/faisalman/ua-parser-js
+var parser = new UAParser();
+var result = parser.getResult();
+console.log(result.browser.name);
+console.log(parseInt(result.browser.version.split('.')[0], 10));
+console.log(result.device.type);
+console.log(result.os.name);
+console.log(result.os.version);
+
+
 jQuery( document ).ready( function( $ ) {

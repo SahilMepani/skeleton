232ce4fa1eff7eafe31353b7fae930f0bb810fae
Sahil <productreseller@gmail.com>
2019-11-15 17:10:14 +0530

    Updates

--- templates/styleguide.php
+++ templates/styleguide.php
@@ -23,34 +23,34 @@
   <p>Bootstrap's grid system is responsive, and the columns will re-arrange depending on the screen size: On a big screen it might look better with the content organized in three columns, but on a small screen it would be better if the content items were stacked on top of each other.</p>
   <p>Remember that grid columns should add up to twelve for a row. More than that, columns will stack no matter the viewport.</p>
 
-  <code class="d-inline-block p-1 mb-1">&lt;div class="col-1"&gt;</code>
+  <code class="p-1 mb-1">&lt;div class="col-1"&gt;</code>
   <div class="row text-center mb-3">
     <div class="col-1">
       <div class="bg-grey p-2">Hi</div>
     </div>
   </div> <!-- .row -->
 
-  <code class="d-inline-block p-1 mb-1">&lt;div class="col-2"&gt;</code>
+  <code class="p-1 mb-1">&lt;div class="col-2"&gt;</code>
   <div class="row text-center mb-3">
     <div class="col-2"><div class="bg-grey p-2">Hi</div></div>
   </div> <!-- .row -->
 
-  <code class="d-inline-block p-1 mb-1">&lt;div class="col-3"&gt;</code>
+  <code class="p-1 mb-1">&lt;div class="col-3"&gt;</code>
   <div class="row text-center mb-3">
     <div class="col-3"><div class="bg-grey p-2">Hi</div></div>
   </div> <!-- .row -->
 
-  <code class="d-inline-block p-1 mb-1">&lt;div class="col-4"&gt;</code>
+  <code class="p-1 mb-1">&lt;div class="col-4"&gt;</code>
   <div class="row text-center mb-3">
     <div class="col-4"><div class="bg-grey p-2">Hi</div></div>
   </div> <!-- .row -->
 
-  <code class="d-inline-block p-1 mb-1">&lt;div class="col-5"&gt;</code>
+  <code class="p-1 mb-1">&lt;div class="col-5"&gt;</code>
   <div class="row text-center mb-3">
     <div class="col-5"><div class="bg-grey p-2">Hi</div></div>
   </div> <!-- .row -->
 
-  <code class="d-inline-block p-1 mb-1">&lt;div class="col-6"&gt;</code>
+  <code class="p-1 mb-1">&lt;div class="col-6"&gt;</code>
   <div class="row text-center mb-3">
     <div class="col-6"><div class="bg-grey p-2">Hi</div></div>
   </div> <!-- .row -->
@@ -78,13 +78,35 @@
       <h5><code>&lt;h5&gt;</code> Heading</h5>
 
       <h6><code>&lt;h6&gt;</code> Heading</h6>
+
+      <p>We can interchange heading tag with different heading class. For example. <code class="alert-info">&lt;h5 class="h1"&gt;</code> It will use h5 tag but h1 styling will be applied. Generally useful for SEO.</p>
     </div> <!-- .col -->
 
     <div class="col-md">
 
-      <p>We can interchange heading tag with different heading class. For example. <code class="alert-info">&lt;h5 class="h1"&gt;</code> It will use h5 tag but h1 styling will be applied. Generally useful for SEO.</p>
-
       <p>You might have noticed all the monospaced text in this guide. There are a number of inline <abbr title="HyperText Markup Language">HTML</abbr> elements you may use anywhere within other elements, including <abbr title="Abbreviation">abbr</abbr>, <cite>cite</cite>, <code>code</code>, <del>del</del>, <em>em</em>, <ins>ins</ins>, <strong>strong</strong>, <var>var</var>, and <a href="#" title="link">link</a></p>
+
+      <h4 class="mb-3">Text Alignment</h4>
+
+      <code class="mb-1">&lt;div class="text-left"&gt;</code>
+      <div class="text-left mb-3">
+        <p>This text is align left</p>
+      </div> <!-- .text-left -->
+
+      <code class="mb-1">&lt;div class="text-right"&gt;</code>
+      <div class="text-right mb-3">
+        <p>This text is align right</p>
+      </div> <!-- .text-right -->
+
+      <code class="mb-1">&lt;div class="text-center"&gt;</code>
+      <div class="text-center mb-3">
+        <p>This text is align center</p>
+      </div> <!-- .text-center -->
+
+      <code class="mb-1">&lt;div class="text-justify"&gt;</code>
+      <div class="text-justify mb-3">
+        <p>This text is justified. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, fugit.</p>
+      </div> <!-- .text-center -->
 
     </div> <!-- .col -->
 
@@ -94,7 +116,7 @@
 <hr class="mb-9" />
 
 <h3>Horizontal Rule</h3>
-<code>&lt;hr&gt; OR &lt;hr class="hr"&gt;</code>
+<code>&lt;hr&gt;</code>
 <hr class="mb-9">
 
 <h3 class="mb-4">Lists</h3>
@@ -133,7 +155,7 @@
 
 <div class="row">
   <div class="col">
-    <h5>Definition List List <code>&lt;dl&gt;</code></h5>
+    <h5>Definition List <code>&lt;dl&gt;</code></h5>
     <dl>
        <dt>Definition list</dt>
        <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
@@ -351,83 +373,103 @@
 <hr class="mb-9">
 
 
-<h3 class="mb-3">Image Alignments</h3>
+<h3 class="mb-3">Spacing</h3>
 
-<div class="row">
+<div class="row mb-4">
 
   <div class="col">
-    <h5>Left <code>&lt;img class="align-left"&gt;</code></h5>
-    <div class="mb-2">
-      <img src="http://placehold.it/150/ff2500/ffffff&text=368" class="align-left">
-    </div> <!-- .mb-2 -->
+    <p>The margin classes are used to create space around elements, outside of any defined borders. There are classes for setting the margin for each side of an element (top, right, bottom, and left).</p>
+
+    <p><b>m-(number)</b> <br /> Set margin for all four sides (top, right, bottom, and left).</p>
+    <p><b>mt-(number)</b> <br /> Sets the top margin of an element</p>
+    <p><b>mr-(number)</b> <br /> Sets the right margin of an element</p>
+    <p><b>mb-(number)</b> <br /> Sets the bottom margin of an element</p>
+    <p><b>ml-(number)</b> <br /> Sets the left margin of an element</p>
+
+    <code class="mb-1">&lt;div class="mb-3"&gt;</code>
+    <div class="bg-grey mb-3">I've a margin bottom of 30px</div>
+
+    <code class="mb-1">&lt;div class="mb-2"&gt;</code>
+    <div class="bg-grey mb-2">I've a margin bottom of 20px</div>
+
+    <div class="bg-grey">Dummy Text</div>
+
+
+    <div class="bg-grey mt-4 mr-3 mb-2 ml-1">
+      <code class="mb-1">&lt;div class="mt-4 mt-3 mb-2 ml-1"&gt;</code> <br />
+      <p>I've a margin of 40px at top, 30px at right, 20px at bottom and 10px at left</p>
+    </div>
+
+    <div class="bg-grey">Dummy Text</div>
   </div> <!-- .col -->
 
   <div class="col">
-    <h5>Right <code>&lt;img class="align-right"&gt;</code></h5>
-    <div class="mb-2">
-      <img src="http://placehold.it/150/ff2500/ffffff&text=368" class="align-right">
-    </div> <!-- .mb-2 -->
+    <p>The padding classes are used to generate space around an element's content, inside of any defined borders. There are classes for setting the classes for each side of an element (top, right, bottom, and left).</p>
+
+    <p><b>p-(number)</b> <br /> Set padding for all four sides (top, right, bottom, and left).</p>
+    <p><b>pt-(number)</b> <br /> Sets the top padding of an element</p>
+    <p><b>pr-(number)</b> <br /> Sets the right padding of an element</p>
+    <p><b>pb-(number)</b> <br /> Sets the bottom padding of an element</p>
+    <p><b>pl-(number)</b> <br /> Sets the left padding of an element</p>
+
+    <code class="mb-1">&lt;div class="p-3"&gt;</code>
+    <div class="bg-grey p-3 mb-2">I've a padding of 30px at each side</div>
+
+    <code class="mb-1">&lt;div class="pb-2"&gt;</code>
+    <div class="bg-grey p-2 mb-2">I've a padding of 20px at each side</div>
+
+    <code class="mb-1">&lt;div class="pt-2 pr-1-5 pb-3 pl-2-5"&gt;</code>
+    <div class="bg-grey pt-2 pr-1-5 pb-3 pl-2-5 mb-2"><p>I've a padding of 20px at top, 15px at right, 50px at bottom and 25px at left</p></div>
   </div> <!-- .col -->
 
 </div> <!-- .row -->
 
-<h5>Center <code>&lt;img class="align-center"&gt;</code></h5>
-<div class="mb-2">
-  <img src="http://placehold.it/150/ff2500/ffffff&text=368" class="alignright">
-</div> <!-- .mb-2 -->
+<p>(number) can be replaced by any number starting from 0-5 to 20 at an interval of .5. For eg. 0-5, 1, 1-5, 2, 2-5 etc.</p>
 
-<hr class="hr">
+<p>Each number gets multiplied by 10px. So a class "mr-2" will set margin of 20px to the right of the element</p>
 
-<h2>Buttons</h2>
+<hr class="mb-9">
+
+
+<h3 class="mb-3">Buttons</h3>
 
 <div class="row">
+
   <div class="col">
-    <h5>Primary</h5>
-    <p>
-      <a href="#" class="btn btn-primary btn-xs">Extra Small</a>
-      <a href="#" class="btn btn-primary btn-sm">Small</a>
-      <a href="#" class="btn btn-primary btn-md">Medium</a>
-      <a href="#" class="btn btn-primary btn-lg">Large</a>
-      <a href="#" class="btn btn-primary btn-xlg">Extra Large</a>
-    </p>
+    <h4>Sizes</h4>
+    <code class="mb-1">&lt;a class="btn btn-xs"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-xs">Extra Small</a></div>
+    <code class="mb-1">&lt;a class="btn btn-sm"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-sm">Small</a></div>
+    <code class="mb-1">&lt;a class="btn btn-md"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-md">Medium</a></div>
+    <code class="mb-1">&lt;a class="btn btn-lg"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-lg">Large</a></div>
+    <code class="mb-1">&lt;a class="btn btn-xlg"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-xlg">Extra Large</a></div>
+    <code class="mb-1">&lt;a class="btn btn-md btn-block"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-md btn-block">Block - Medium</a></div>
+    <p>It will cover the entire width of the container.</p>
   </div> <!-- .col -->
 
   <div class="col">
-    <h5>Secondary</h5>
-    <p>
-      <a href="#" class="btn btn-secondary btn-xs">Extra Small</a>
-      <a href="#" class="btn btn-secondary btn-sm">Small</a>
-      <a href="#" class="btn btn-secondary btn-md">Medium</a>
-      <a href="#" class="btn btn-secondary btn-lg">Large</a>
-      <a href="#" class="btn btn-secondary btn-xlg">Extra Large</a>
-    </p>
+    <h4>Colors</h4>
+
+    <code class="mb-1">&lt;a class="btn btn-primary btn-md"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-primary btn-md">Medium</a></div>
+
+    <code class="mb-1">&lt;a class="btn btn-secondary btn-lg"&gt;</code>
+    <div class="mb-2"><a href="#" class="btn btn-secondary btn-lg">Large</a></div>
+
+    <p>We can mix and match the sizes and color classes.</p>
+
   </div> <!-- .col -->
 </div> <!-- .row -->
 
-<hr class="hr">
+<hr class="mb-9">
 
-<h2>Social Icons (With Tooltips)</h2>
 
-<ul class="list-socials clearfix">
-  <li class="icon-facebook"><a href="<?php echo @$facebook; ?>"><span class="tooltip">@tooltip</span>Facebook</a></li>
-  <li class="icon-twitter"><a href="<?php echo @$twitter; ?>"><span class="tooltip">@tooltip</span>Twitter</a></li>
-  <li class="icon-instagram"><a href="<?php echo @$instagram; ?>"><span class="tooltip">@tooltip</span>Instagram</a></li>
-  <li class="icon-gplus"><a href="<?php echo @$instagram; ?>"><span class="tooltip">@tooltip</span>Google Plus</a></li>
-  <li class="icon-pinterest"><a href="<?php echo @$instagram; ?>"><span class="tooltip">@tooltip</span>Pinterest</a></li>
-  <li class="icon-youtube"><a href="<?php echo @$instagram; ?>"><span class="tooltip">@tooltip</span>Youtube</a></li>
-  <li class="icon-linkedin"><a href="<?php echo @$instagram; ?>"><span class="tooltip">@tooltip</span>Linkedin</a></li>
-</ul> <!-- .list-socials -->
-
-<hr class="hr">
-
-<h2>Bootstrap Grid <em>(only)</em></h2>
-<p>Please refer to <a href="http://getbootstrap.com/css/">http://getbootstrap.com/css/#grid</a></p>
-
-<hr class="hr">
-
-<h2>Gravity Form</h2>
-
-<?php gravity_form(1, $display_title=false, $display_description=false, $display_inactive=false, $field_values=null, $ajax=true, $tabindex=10); ?>
+<?php //gravity_form(1, $display_title=false, $display_description=false, $display_inactive=false, $field_values=null, $ajax=true, $tabindex=10); ?>
 
 </div> <!-- .container-fluid -->
 

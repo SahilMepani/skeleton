a3d7fbc8870ac449d54fa7c6b84111049f787be3
Sahil Mepani <productreseller@gmail.com>
2020-04-08 11:02:39 +0530

    Remove some files

--- source/js/custom/!document.ready.js
+++ source/js/custom/!document.ready.js
@@ -1,14 +1,1 @@
-/*=================================
-=            UA Parser            =
-=================================*/
-// https://github.com/faisalman/ua-parser-js
-var parser = new UAParser();
-var result = parser.getResult();
-console.log(result.browser.name);
-console.log(parseInt(result.browser.version.split('.')[0], 10));
-console.log(result.device.type);
-console.log(result.os.name);
-console.log(result.os.version);
-
-
 jQuery( document ).ready( function( $ ) {

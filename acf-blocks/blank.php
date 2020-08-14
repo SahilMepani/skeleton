f2c57276855d6e16e0efb14de97d6be770e699f4
Sahil Mepani <productreseller@gmail.com>
2020-08-14 17:41:43 +0530

    Update

--- acf-blocks/blank.php
+++ acf-blocks/blank.php
@@ -6,12 +6,7 @@
   $padding_bottom       = get_field( 'padding_bottom' );
   $custom_classes       = get_field( 'custom_classes' );
   $custom_css           = get_field( 'custom_css' );
-  $unique_id            = get_field( 'unique_id' );
-  $remove_border_bottom = get_field('remove_border_bottom');
-  if ( $remove_border_bottom ) {
-    $remove_border_bottom = 'border-bottom: 0';
-  }
 ?>
 
-<section class="<?php echo $bg_color; ?> <?php echo $padding_top; ?> <?php echo $padding_bottom; ?> <?php echo $custom_classes; ?>" style="<?php echo $remove_border_bottom; ?> <?php echo $custom_css; ?>">
+<section class="<?php echo $bg_color; ?> <?php echo $padding_top; ?> <?php echo $padding_bottom; ?> <?php echo $custom_classes; ?>" style="<?php echo $custom_css; ?>">
 </section>

8098be97cdedaf9bf86991fe01c47a10e04cc134
Sahil Mepani <productreseller@gmail.com>
2022-03-30 12:58:40 +0530

    Updates

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ require_once( get_template_directory() . '/functions/enqueue-scripts.php' );
 require_once( get_template_directory() . '/functions/remove-junk-from-head.php' );
 require_once( get_template_directory() . '/functions/wp-plugins.php' );
 require_once( get_template_directory() . '/functions/security.php' );
+require_once( get_template_directory() . '/functions/remove-comments.php' );
 // require_once( get_template_directory() . '/functions/custom-post-types.php' );
 // require_once( get_template_directory() . '/functions/custom-admin-columns.php' );
 // require_once( get_template_directory() . '/functions/admin-ajax.php' );

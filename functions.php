63f44dd72080abf31ba65819f7322740d0215ccb
Sahil Mepani <productreseller@gmail.com>
2023-01-12 00:53:17 +0530

    Update

--- functions.php
+++ functions.php
@@ -1,24 +1,24 @@
 <?php
 
 // require_once( get_template_directory() . '/functions/shortcodes.php' );
-require_once( get_template_directory() . '/functions/miscellaneous.php' );
-require_once( get_template_directory() . '/functions/helpers.php' );
-require_once( get_template_directory() . '/functions/disable-auto-embed-script.php' );
-require_once( get_template_directory() . '/functions/disable-wp-generated-image-sizes.php' );
-require_once( get_template_directory() . '/functions/add-image-sizes.php' );
-require_once( get_template_directory() . '/functions/custom-login.php' );
-require_once( get_template_directory() . '/functions/register-acf-blocks.php' );
-require_once( get_template_directory() . '/functions/block-editor-settings.php' );
-require_once( get_template_directory() . '/functions/enqueue-scripts.php' );
-require_once( get_template_directory() . '/functions/remove-junk-from-head.php' );
-require_once( get_template_directory() . '/functions/wp-plugins.php' );
-require_once( get_template_directory() . '/functions/security.php' );
-require_once( get_template_directory() . '/functions/remove-comments.php' );
-require_once( get_template_directory() . '/functions/acf-options-page.php' );
-require_once( get_template_directory() . '/functions/remove-post-type.php' );
-require_once( get_template_directory() . '/functions/allowed-blocks.php' );
-require_once( get_template_directory() . '/functions/disable-blocks-directory.php' );
-require_once( get_template_directory() . '/functions/skip-dashboard.php' );
+require_once get_template_directory() . '/functions/miscellaneous.php';
+require_once get_template_directory() . '/functions/helpers.php';
+require_once get_template_directory() . '/functions/disable-auto-embed-script.php';
+require_once get_template_directory() . '/functions/disable-wp-generated-image-sizes.php';
+require_once get_template_directory() . '/functions/add-image-sizes.php';
+require_once get_template_directory() . '/functions/custom-login.php';
+require_once get_template_directory() . '/functions/register-acf-blocks.php';
+require_once get_template_directory() . '/functions/block-editor-settings.php';
+require_once get_template_directory() . '/functions/enqueue-scripts.php';
+require_once get_template_directory() . '/functions/remove-junk-from-head.php';
+require_once get_template_directory() . '/functions/wp-plugins.php';
+require_once get_template_directory() . '/functions/security.php';
+require_once get_template_directory() . '/functions/remove-comments.php';
+require_once get_template_directory() . '/functions/acf-options-page.php';
+require_once get_template_directory() . '/functions/remove-post-type.php';
+require_once get_template_directory() . '/functions/allowed-blocks.php';
+require_once get_template_directory() . '/functions/disable-blocks-directory.php';
+require_once get_template_directory() . '/functions/skip-dashboard.php';
 // require_once( get_template_directory() . '/functions/custom-post-types.php' );
 // require_once( get_template_directory() . '/functions/custom-admin-columns.php' );
 // require_once( get_template_directory() . '/functions/admin-ajax.php' );
@@ -30,34 +30,30 @@ require_once( get_template_directory() . '/functions/skip-dashboard.php' );
 ////////////////////////////////////////////////
 // define('WP_POST_REVISIONS', 5);
 
-
 // Register menus
 ////////////////////////////////////////////////
 register_nav_menus(
-  array(
-    'header-menu' => 'Header Menu',
-    'footer-menu' => 'Footer Menu',
-  )
+	array(
+		'header-menu' => 'Header Menu',
+		'footer-menu' => 'Footer Menu'
+	)
 );
 
 // Add except to page post type
 /* ========================================== */
 // add_post_type_support( 'page', 'excerpt' );
 
-
 // Enable featured images for all post types including custom
 ////////////////////////////////////////////////
 // add_theme_support('post-thumbnails');
-
 
 // Faster WordPress
 // Disable if Autoptimize is enabled
 /* ========================================== */
 function skel_hints() {
-  header("link: <" . get_stylesheet_uri() . ">;");
+	header( "link: <" . get_stylesheet_uri() . ">;" );
 }
-add_action('send_headers', 'skel_hints');
-
+add_action( 'send_headers', 'skel_hints' );
 
 // Load MO files
 // file name should only be {locale}.mo/po
@@ -66,7 +62,6 @@ add_action('send_headers', 'skel_hints');
 // function wpdocs_theme_setup(){
 //   load_theme_textdomain( 'skel', get_template_directory().'/lang' );
 // }
-
 
 /*=========================================
 =            Register sidebars            =
@@ -84,8 +79,6 @@ add_action('send_headers', 'skel_hints');
 //   );
 // endforeach;
 
-
-
 /*----------  REQUIRED - Do not edit  ----------*/
 
 /*=============================================
@@ -93,9 +86,8 @@ add_action('send_headers', 'skel_hints');
 =============================================*/
 add_editor_style();
 
-
 /*============================================================
 =            Overrides default image-URL behavior            =
 ============================================================*/
 // http://wordpress.org/support/topic/insert-image-default-to-no-link
-update_option('image_default_link_type', 'none');
+update_option( 'image_default_link_type', 'none' );
--- header.php
+++ header.php
@@ -1,52 +1,58 @@
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
+
 <head>
-  <!-- HTML Boilerplte v8.00 -->
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title><?php wp_title( '-', true, 'right' ); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png" type="image/png">
-  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-  <!-- remove below link if google fonts are not used -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <!-- End - Google fonts -->
-  <link rel="profile" href="http://gmpg.org/xfn/11" />
-  <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-  <?php endif; ?>
-  <?php wp_head(); ?>
+	<!-- HTML Boilerplte v8.00 -->
+	<meta charset="<?php bloginfo( 'charset' );?>" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title><?php wp_title( '-', true, 'right' );?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png" type="image/png">
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+	<!-- remove below link if google fonts are not used -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<!-- End - Google fonts -->
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ): ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' );?>">
+	<?php endif;?>
+	<?php wp_head();?>
 </head>
 
-<body <?php body_class( 'site-wrapper' ); ?>>
+<body <?php body_class( 'site-wrapper' );?>>
 
-<?php wp_body_open(); ?>
+	<?php wp_body_open();?>
 
-<header class="site-header clearfix" role="banner">
-  <div class="container-fluid">
+	<header class="site-header clearfix" role="banner">
+		<div class="container-fluid">
 
-    <div class="header-logo">
-      <a href="<?php echo esc_url( home_url() ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ) ); ?>" aria-label="Go to Home" <?php echo (is_front_page()) ? 'aria-current="page"' : ''; ?>>
-        <span><?php bloginfo('description'); ?></span>
-        <?php include('images/svg/header-logo.svg') ?>
-      </a>
-    </div>
+			<div class="header-logo">
+				<a href="<?php echo esc_url( home_url() ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ) );?>"
+					aria-label="Go to Home" <?php echo ( is_front_page() ) ? 'aria-current="page"' : ''; ?>>
+					<span><?php bloginfo( 'description' );?></span>
+					<?php include 'images/svg/header-logo.svg'?>
+				</a>
+			</div>
 
-    <button class="header-nav-toggle btn" aria-label="<?php _e( 'show primary navigation', 'skel' ); ?>" aria-haspopup="true" aria-expanded="false" aria-controls="siteMenu"><i class="i-font-before i-menu"></i><?php _e( 'Menu', 'skel' ); ?></button>
+			<button class="header-nav-toggle btn" aria-label="<?php _e( 'show primary navigation', 'skel' );?>"
+				aria-haspopup="true" aria-expanded="false" aria-controls="siteMenu"><i
+					class="i-font-before i-menu"></i><?php _e( 'Menu', 'skel' );?></button>
 
-    <nav class="header-nav" role="navigation" aria-label="<?php _e( 'primary navigation', 'skel' ); ?>">
-      <button class="header-nav-close" aria-label="<?php _e( 'close primary navigation' ); ?>" aria-haspopup="true" aria-expanded="true" aria-controls="siteMenu"><i class="i-font-before i-x"></i></button>
-      <?php wp_nav_menu( array(
-        'theme_location' => 'header-menu',
-        'container' => 'false',
-        'menu_class' => 'header-nav-parent-menu',
-        'items_wrap'	=> '<ul id="siteMenu" class="%2$s">%3$s</ul>',
-        )
-      ); ?>
-    </nav> <!-- .header-nav -->
+			<nav class="header-nav" role="navigation" aria-label="<?php _e( 'primary navigation', 'skel' );?>">
+				<button class="header-nav-close" aria-label="<?php _e( 'close primary navigation' );?>"
+					aria-haspopup="true" aria-expanded="true" aria-controls="siteMenu"><i
+						class="i-font-before i-x"></i></button>
+				<?php wp_nav_menu( [
+						'theme_location' => 'header-menu',
+						'container'      => 'false',
+						'menu_class'     => 'header-nav-parent-menu',
+						'items_wrap'     => '<ul id="siteMenu" class="%2$s">%3$s</ul>'
+					]
+				);?>
+			</nav> <!-- .header-nav -->
 
-  </div> <!-- .container-fluid -->
-</header> <!-- .site-header -->
+		</div> <!-- .container-fluid -->
+	</header> <!-- .site-header -->
 
-<main class="site-content" role="main" >
+	<main class="site-content" role="main">

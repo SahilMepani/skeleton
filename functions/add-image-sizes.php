e5535b23c7302bbf1cf85e7b7eec5482e283b350
Sahil Mepani <productreseller@gmail.com>
2023-01-18 17:11:01 +0530

    Update media sizes

--- functions/add-image-sizes.php
+++ functions/add-image-sizes.php
@@ -2,9 +2,6 @@
 // Add custom image sizes
 ////////////////////////////////////////////////
 // add_image_size( 'blog_featured_thumb', width, height, crop );
-add_image_size( 'h200', 9999, 200 ); // used for image acf preview
-add_image_size( 'w200', 200, 9999 ); // used for image acf preview
-add_image_size( 'w414', 414, 9999 );
 add_image_size( 'w768', 768, 9999 );
 add_image_size( 'w1400', 1400, 9999 );
 add_image_size( 'w1920', 1920, 9999 );

cc9da1b22c6a3647fae966515f59ff089b6cf251
Sahil Mepani <productreseller@gmail.com>
2023-02-18 21:37:04 +0530

    Update

--- functions/add-image-sizes.php
+++ functions/add-image-sizes.php
@@ -4,7 +4,6 @@
 ////////////////////////////////////////////////
 // add_image_size( 'blog_featured_thumb', width, height, crop );
 add_image_size( 'h200', 9999, 200 );
-add_image_size( 'w200', 200, 9999 );
 add_image_size( 'w768', 768, 9999 );
 add_image_size( 'w1400', 1400, 9999 );
 add_image_size( 'w1920', 1920, 9999 );

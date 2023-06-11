da0b7f63ee4d3883a67d482b52cfe6711b37ed47
Sahil Mepani <productreseller@gmail.com>
2023-06-12 01:35:00 +0530

    Update

--- source/js/custom/miscellaneous.js
+++ source/js/custom/miscellaneous.js
@@ -1,12 +1,15 @@
-// Check if the user's screen primary input device is touch or not
-// https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
-////////////////////////////////////////////////
+/**
+ * Check if the user's screen primary input device is touch or not
+ * https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
+ */
 if (!window.matchMedia("(pointer: coarse)").matches) {
 	document.documentElement.classList.add("js-no-touchevents")
 }
 
-// Find the header height and set to scroll-padding-top css property
-////////////////////////////////////////////////
+/**
+ * Find the header height and set to
+ * scroll-padding-top css property
+ */
 const resizeHeaderHeight = () => {
 	const headerHeight = document.querySelector(".site-header").offsetHeight
 	document.documentElement.style.setProperty(
@@ -17,8 +20,9 @@ const resizeHeaderHeight = () => {
 resizeHeaderHeight()
 window.addEventListener("resize", resizeHeaderHeight)
 
-// Check if user scrolled
-////////////////////////////////////////////////
+/**
+ * Check if user scrolled
+ */
 let pageScroll = function () {
 	let scroll = window.scrollY
 	const siteHeader = document.querySelector(".site-header")
@@ -34,3 +38,21 @@ let pageScroll = function () {
 pageScroll()
 // run on scroll
 window.addEventListener("scroll", pageScroll)
+
+/**
+ * Scroll to a specific section from #ID at the end of Window URL
+ */
+const current_url = window.location.href
+if (current_url.indexOf("#") != -1) {
+	let hash_index = current_url.indexOf("#")
+	element_id = current_url.substring(hash_index + 1)
+	let element = document.getElementById(element_id)
+	if (element) {
+		setTimeout(function () {
+			element.scrollIntoView({
+				behavior: "smooth",
+				block: "center"
+			})
+		}, 100)
+	}
+}

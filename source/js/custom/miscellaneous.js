a8adc057c052a48236fc2396b457783179990f78
Sahil Mepani <productreseller@gmail.com>
2023-10-02 09:53:38 +0530

    Update

--- source/js/custom/miscellaneous.js
+++ source/js/custom/miscellaneous.js
@@ -2,8 +2,8 @@
  * Check if the user's screen primary input device is touch or not
  * https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
  */
-if (!window.matchMedia("(pointer: coarse)").matches) {
-	document.documentElement.classList.add("js-no-touchevents")
+if (!window.matchMedia('(pointer: coarse)').matches) {
+	document.documentElement.classList.add('js-no-touchevents');
 }
 
 /**
@@ -11,48 +11,48 @@ if (!window.matchMedia("(pointer: coarse)").matches) {
  * scroll-padding-top css property
  */
 const resizeHeaderHeight = () => {
-	const headerHeight = document.querySelector(".site-header").offsetHeight
+	const headerHeight = document.querySelector('.site-header').offsetHeight;
 	document.documentElement.style.setProperty(
-		"--scroll-padding-top",
-		headerHeight + "px"
-	)
-}
-resizeHeaderHeight()
-window.addEventListener("resize", resizeHeaderHeight)
+		'--scroll-padding-top',
+		headerHeight + 'px'
+	);
+};
+resizeHeaderHeight();
+window.addEventListener('resize', resizeHeaderHeight);
 
 /**
  * Check if user scrolled
  */
 let pageScroll = function () {
-	let scroll = window.scrollY
-	const siteHeader = document.querySelector(".site-header")
+	let scroll = window.scrollY;
+	const siteHeader = document.querySelector('.site-header');
 	if (siteHeader) {
 		if (scroll > 0) {
-			siteHeader.classList.add("js-fixed")
+			siteHeader.classList.add('js-fixed');
 		} else {
-			siteHeader.classList.remove("js-fixed")
+			siteHeader.classList.remove('js-fixed');
 		}
 	}
-}
+};
 // need to run once, as sometimes the page is already scrolled down on load
-pageScroll()
+pageScroll();
 // run on scroll
-window.addEventListener("scroll", pageScroll)
+window.addEventListener('scroll', pageScroll);
 
 /**
  * Scroll to a specific section from #ID at the end of Window URL
  */
-const current_url = window.location.href
-if (current_url.indexOf("#") != -1) {
-	let hash_index = current_url.indexOf("#")
-	element_id = current_url.substring(hash_index + 1)
-	let element = document.getElementById(element_id)
+const current_url = window.location.href;
+if (current_url.indexOf('#') != -1) {
+	let hash_index = current_url.indexOf('#');
+	element_id = current_url.substring(hash_index + 1);
+	let element = document.getElementById(element_id);
 	if (element) {
 		setTimeout(function () {
 			element.scrollIntoView({
-				behavior: "smooth",
-				block: "center"
-			})
-		}, 100)
+				behavior: 'smooth',
+				block: 'center',
+			});
+		}, 100);
 	}
 }

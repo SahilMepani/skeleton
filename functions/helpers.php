75734999c698d52fdddabe434dae56c49a700643
Sahil Mepani <productreseller@gmail.com>
2024-04-21 11:12:11 +0530

    Update

--- functions/helpers.php
+++ functions/helpers.php
@@ -13,11 +13,11 @@
  */
 function skel_swiper_direction_class()
 {
-    if ( ICL_LANGUAGE_CODE == 'en' ) {
-        return 'swiper';
-    } else if ( ICL_LANGUAGE_CODE == 'ar' ) {
-        return 'swiper-rtl';
-    }
+	if (ICL_LANGUAGE_CODE == 'en') {
+		return 'swiper';
+	} else if (ICL_LANGUAGE_CODE == 'ar') {
+		return 'swiper-rtl';
+	}
 }
 
 /**
@@ -34,24 +34,24 @@ function skel_swiper_direction_class()
 ////////////////////////////////////////////////
 function skel_direction_class()
 {
-    if ( ICL_LANGUAGE_CODE == 'en' ) {
-        return 'dir-ltr';
-    } else if ( ICL_LANGUAGE_CODE == 'ar' ) {
-        return 'dir-rtl';
-    }
+	if (ICL_LANGUAGE_CODE == 'en') {
+		return 'dir-ltr';
+	} else if (ICL_LANGUAGE_CODE == 'ar') {
+		return 'dir-rtl';
+	}
 }
 
 /**
- * Retrieves the content of an SVG file located in the '/images/svg/' directory
+ * Retrieves the content of an SVG file located in the current active theme '/images/svg/' directory
  *
  * @param  string       $image The name of the SVG file (without the '.svg' extension) to retrieve.
  * @return string|false The content of the SVG file if found, or FALSE if the file does not exist or cannot be read.
  */
-function svg( $image )
+function svg($image)
 {
-    $file = get_template_directory_uri() . '/images/svg/' . $image . '.svg';
+	$file = get_template_directory_uri() . '/images/svg/' . $image . '.svg';
 
-    return file_get_contents( $file );
+	return file_get_contents($file);
 }
 
 /**
@@ -60,16 +60,16 @@ function svg( $image )
  * @param  string $url The YouTube video URL from which to extract the video ID.
  * @return string The valid YouTube video link generated from the provided URL.
  */
-function skel_get_yt_link( $url )
+function skel_get_yt_link($url)
 {
-    // grab the position of forward slash
-    $pos = strrpos( $url, '/' );
-    // use position to get substring
-    $id = $pos === false ? $url : substr( $url, $pos + 1 );
-    // create working youtube link
-    $url = 'https://www.youtube.com/watch?v=' . $id;
+	// grab the position of forward slash
+	$pos = strrpos($url, '/');
+	// use position to get substring
+	$id = $pos === false ? $url : substr($url, $pos + 1);
+	// create working youtube link
+	$url = 'https://www.youtube.com/watch?v=' . $id;
 
-    return $url;
+	return $url;
 }
 
 /**
@@ -78,12 +78,12 @@ function skel_get_yt_link( $url )
  * @param  string       $link The input string containing a potential YouTube video link.
  * @return string|false The valid YouTube video link generated from the provided input string,
  */
-function skel_get_validate_youtube_link( $link )
+function skel_get_validate_youtube_link($link)
 {
-    preg_match( "#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $link, $matches );
-    $link = 'https://www.youtube.com/watch?v=' . $matches[ 0 ];
+	preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $link, $matches);
+	$link = 'https://www.youtube.com/watch?v=' . $matches[0];
 
-    return $link;
+	return $link;
 }
 
 /**
@@ -97,18 +97,18 @@ function skel_get_validate_youtube_link( $link )
  * @param  int    $limit   The maximum number of words in the excerpt.
  * @return string The customized excerpt of the specified post.
  */
-function skel_get_the_excerpt( $post_id, $limit )
+function skel_get_the_excerpt($post_id, $limit)
 {
-    $excerpt = explode( ' ', get_the_excerpt( $post_id ), $limit );
-    if ( count( $excerpt ) >= $limit ) {
-        array_pop( $excerpt );
-        $excerpt = implode( " ", $excerpt ) . '...';
-    } else {
-        $excerpt = implode( " ", $excerpt );
-    }
-    $excerpt = preg_replace( '`\[[^\]]*\]`', '', $excerpt );
+	$excerpt = explode(' ', get_the_excerpt($post_id), $limit);
+	if (count($excerpt) >= $limit) {
+		array_pop($excerpt);
+		$excerpt = implode(" ", $excerpt) . '...';
+	} else {
+		$excerpt = implode(" ", $excerpt);
+	}
+	$excerpt = preg_replace('`\[[^\]]*\]`', '', $excerpt);
 
-    return $excerpt;
+	return $excerpt;
 }
 
 /**
@@ -123,24 +123,24 @@ function skel_get_the_excerpt( $post_id, $limit )
  * @param  int    $length The maximum length of the shortened text.
  * @return string The shortened text string.
  */
-function skel_get_text_shorter( $input, $length )
+function skel_get_text_shorter($input, $length)
 {
-    // no need to trim, already shorter than trim length
-    if ( strlen( $input ) <= $length ) {
-        return $input;
-    }
-    // find last space within length
-    $last_space = strrpos( substr( $input, 0, $length ), ' ' );
-    if ( ! $last_space ) {
-        $last_space = $length;
-    }
+	// no need to trim, already shorter than trim length
+	if (strlen($input) <= $length) {
+		return $input;
+	}
+	// find last space within length
+	$last_space = strrpos(substr($input, 0, $length), ' ');
+	if (!$last_space) {
+		$last_space = $length;
+	}
 
-    $trimmed_text = substr( $input, 0, $last_space );
+	$trimmed_text = substr($input, 0, $last_space);
 
-    //add ellipses (...)
-    $trimmed_text .= '...';
+	//add ellipses (...)
+	$trimmed_text .= '...';
 
-    return $trimmed_text;
+	return $trimmed_text;
 }
 
 /**
@@ -154,14 +154,14 @@ function skel_get_text_shorter( $input, $length )
  * @param  string $separator (Optional) The separator to use between terms. Default is a single space.
  * @return string The terms associated with the specified post, separated by the specified separator.
  */
-function skel_get_the_terms( $post_id, $taxonomy, $separator )
+function skel_get_the_terms($post_id, $taxonomy, $separator)
 {
-    $terms     = get_the_terms( $post_id, $taxonomy ); // returns objects array
-    $ar_term   = wp_list_pluck( $terms, 'name' );
-    $separator = ( $separator ) ? $separator : ' ';
-    $result    = join( $separator, $ar_term ); // join all terms name
+	$terms     = get_the_terms($post_id, $taxonomy); // returns objects array
+	$ar_term   = wp_list_pluck($terms, 'name');
+	$separator = ($separator) ? $separator : ' ';
+	$result    = join($separator, $ar_term); // join all terms name
 
-    return $result;
+	return $result;
 }
 
 /**
@@ -174,20 +174,20 @@ function skel_get_the_terms( $post_id, $taxonomy, $separator )
  * @param  string $taxonomy The taxonomy from which to retrieve the terms.
  * @return array  An array containing the name and ID of each term associated with the specified post.
  */
-function skel_get_the_terms_data( $post_id, $taxonomy )
+function skel_get_the_terms_data($post_id, $taxonomy)
 {
-    $terms   = get_the_terms( $post_id, $taxonomy ); // Returns objects array
-    $ar_term = [  ]; // Initialize an array
-    $i       = 0;
-    if ( $terms ) {
-        foreach ( $terms as $term ) {
-            $ar_term[ $i ][ 'name' ] = $term->name; // Store term name
-            $ar_term[ $i ][ 'id' ]   = $term->term_id; // Store term id
-            $i++;
-        }
-    }
+	$terms   = get_the_terms($post_id, $taxonomy); // Returns objects array
+	$ar_term = []; // Initialize an array
+	$i       = 0;
+	if ($terms) {
+		foreach ($terms as $term) {
+			$ar_term[$i]['name'] = $term->name; // Store term name
+			$ar_term[$i]['id']   = $term->term_id; // Store term id
+			$i++;
+		}
+	}
 
-    return $ar_term;
+	return $ar_term;
 }
 
 /**
@@ -199,11 +199,11 @@ function skel_get_the_terms_data( $post_id, $taxonomy )
  * @param  string|false $phone_number (Optional) The phone number to generate the URL for. If not provided or false, an empty tel: URL is returned.
  * @return string       The URL for the phone number in the format tel:phonenumber.
  */
-function skel_get_phone_url( $phone_number = false )
+function skel_get_phone_url($phone_number = false)
 {
-    $phone_number = str_replace( [ '(', ')', '-', '.', '|', ' ' ], '', $phone_number );
+	$phone_number = str_replace(['(', ')', '-', '.', '|', ' '], '', $phone_number);
 
-    return esc_url( 'tel:' . $phone_number );
+	return esc_url('tel:' . $phone_number);
 }
 
 // Limit WP Revisions
@@ -228,26 +228,33 @@ function skel_get_phone_url( $phone_number = false )
  * @param  int         $id The ID of the page to retrieve and insert into another page.
  * @return string|null The content of the specified page, or null if the page with the provided ID does not exist.
  */
-function skel_insert_page( $id )
+function skel_insert_page($id)
 {
-    $post = get_post( $id );
-    if ( $post ) {
-        $content = apply_filters( 'the_content', $post->post_content );
+	$post = get_post($id);
+	if ($post) {
+		$content = apply_filters('the_content', $post->post_content);
 
-        return $content;
-    } else {
-        return null;
-    }
+		return $content;
+	} else {
+		return null;
+	}
 }
 
-// Faster WordPress
-// Disable if Autoptimize is enabled
-/* ========================================== */
+/**
+ * Provide hints for faster WordPress loading.
+ *
+ * Adds a link header to provide hints for faster WordPress loading.
+ * It includes a link to the current theme's stylesheet URI.
+ *
+ * Should be disabled if Autoptimize is enabled to avoid conflicts.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Link
+ */
 function skel_hints()
 {
-    header( "link: <" . get_stylesheet_uri() . ">;" );
+	header("link: <" . get_stylesheet_uri() . ">;");
 }
-add_action( 'send_headers', 'skel_hints' );
+add_action('send_headers', 'skel_hints');
 
 // Load MO files
 // file name should only be {locale}.mo/po
@@ -259,8 +266,10 @@ add_action( 'send_headers', 'skel_hints' );
 
 /*----------  REQUIRED - Do not edit  ----------*/
 
-/*============================================================
-=            Overrides default image-URL behavior            =
-============================================================*/
-// http://wordpress.org/support/topic/insert-image-default-to-no-link
-update_option( 'image_default_link_type', 'none' );
+/**
+ * Overrides the default behavior of image URLs.
+ *
+ * With this change, when inserting images into posts or pages,
+ * they will default to having no link associated with them.
+ */
+update_option('image_default_link_type', 'none');

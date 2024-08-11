95cf50c7b1dc7b4cb917c2252b4290cad04f1ead
Sahil Mepani <productreseller@gmail.com>
2024-08-11 13:20:55 +0530

    Updat

--- functions/section-library.php
+++ functions/section-library.php
@@ -10,7 +10,7 @@
  * @global
  */
 global $core_sections;
-$core_sections = [];
+$core_sections = array();
 
 /**
  * Retrieves the full list of sections.
@@ -18,16 +18,19 @@ $core_sections = [];
  * @since 2.11.0
  * @internal
  *
- * @param boolean [$slug_only=false]
+ * @param boolean [ $slug_only=false]
  * @return array[]|string[]
  */
 function core_get_sections( $slug_only = false ) {
 	global $core_sections;
 
 	if ( $slug_only ) {
-		return array_map( function( $section ) {
-			return $section['slug'];
-		}, array_values( $core_sections ) );
+		return array_map(
+			function ( $section ) {
+				return $section['slug'];
+			},
+			array_values( $core_sections )
+		);
 	} else {
 		return $core_sections;
 	}
@@ -60,13 +63,13 @@ function core_get_section( $section_slug ) {
 function core_add_standard_section_fields( &$reg_data ) {
 
 	// Save the section fields to be used later.
-	$generic_fields = [];
+	$generic_fields = array();
 
 	// Save the generic fields.
 	if ( ! empty( $generic_fields ) ) {
-		array_unshift( $reg_data['fields'], [ 'tab', 'Contents' ] );
+		array_unshift( $reg_data['fields'], array( 'tab', 'Contents' ) );
 
-		$reg_data['fields'][] = [ 'tab', 'Settings' ];
+		$reg_data['fields'][] = array( 'tab', 'Settings' );
 
 		$reg_data['fields'] = array_merge( $reg_data['fields'], $generic_fields );
 	}
@@ -82,51 +85,66 @@ function core_scan_sections() {
 	global $core_sections;
 
 	// Scan for configuration files and register sections.
-	$sections_list = core_scan_dir( get_template_directory() . '/acf-blocks/', 'config.php', function( $file ) {
-		$reg_data = include_once $file['path'];
+	$sections_list = core_scan_dir(
+		get_template_directory() . '/acf-blocks/',
+		'config.php',
+		function ( $file ) {
+			$reg_data = include_once $file['path'];
 
-		$reg_data['slug'] = core_default( 'slug', $reg_data, $file['dirname'] );
-		$reg_data['location'] = $file['info']['dirname'];
-		$reg_data['location_rel'] = $file['path_rel'];
-		$reg_data['includes_preview_image'] = is_file( "{$reg_data['location']}/preview.jpg" );
-		$reg_data['multiple'] = core_default( 'multiple', $reg_data, true );
+			$reg_data['slug']                   = core_default( 'slug', $reg_data, $file['dirname'] );
+			$reg_data['location']               = $file['info']['dirname'];
+			$reg_data['location_rel']           = $file['path_rel'];
+			$reg_data['includes_preview_image'] = is_file( "{$reg_data['location']}/preview.jpg" );
+			$reg_data['multiple']               = core_default( 'multiple', $reg_data, true );
 
-		// Prepare the section fields.
-		$reg_data['fields'] = core_default( 'fields', $reg_data, [] );
+			// Prepare the section fields.
+			$reg_data['fields'] = core_default( 'fields', $reg_data, array() );
 
-		core_add_standard_section_fields( $reg_data );
+			core_add_standard_section_fields( $reg_data );
 
-		// Add the section title.
-		array_unshift( $reg_data['fields'],
-			[ 'accordion', $reg_data['title'], [ 'open' => 1 ] ]
-		);
+			// Add the section title.
+			array_unshift(
+				$reg_data['fields'],
+				array( 'accordion', $reg_data['title'], array( 'open' => 1 ) )
+			);
 
-		// Store the Block Editor version of this section.
-		$reg_data['block'] = [
-			'name' => $reg_data['slug'],
-			'title' => $reg_data['title'],
-			'description' => core_default( 'description', $reg_data, '' ),
-			'icon' => $reg_data['icon'] ?? 'layout',
-			'keywords' => core_default( 'keywords', $reg_data, [] )
-		];
+			// Store the Block Editor version of this section.
+			$reg_data['block'] = array(
+				'name'        => $reg_data['slug'],
+				'title'       => $reg_data['title'],
+				'description' => core_default( 'description', $reg_data, '' ),
+				'icon'        => $reg_data['icon'] ?? 'layout',
+				'keywords'    => core_default( 'keywords', $reg_data, array() ),
+			);
 
-		return $reg_data;
-	});
+			return $reg_data;
+		}
+	);
 
 	// Sort the sections list by section name.
-	usort( $sections_list, function( $a, $b ) {
-		if ( $a['name'] > $b['name'] ) return 1;
-		if ( $a['name'] < $b['name'] ) return -1;
-		return 0;
-	});
+	usort(
+		$sections_list,
+		function ( $a, $b ) {
+			if ( $a['name'] > $b['name'] ) {
+				return 1;
+			}
+			if ( $a['name'] < $b['name'] ) {
+				return -1;
+			}
+			return 0;
+		}
+	);
 
 	// Add an alert for sections that do not have a preview image.
-	$missing_previews = array_filter( $sections_list, function( $section ) {
-		return ! $section['includes_preview_image'];
-	});
+	$missing_previews = array_filter(
+		$sections_list,
+		function ( $section ) {
+			return ! $section['includes_preview_image'];
+		}
+	);
 
 	if ( ! empty( $missing_previews ) ) {
-		$info = [];
+		$info = array();
 		foreach ( $missing_previews as $section ) {
 			$info[] = "{$section['name']} (<code>{$section['slug']}</code>)";
 		}
@@ -153,20 +171,23 @@ function core_register_sections_as_blocks() {
 	global $core_sections;
 
 	foreach ( $core_sections as $section ) {
-		$block_info = $section['block'];
+		$block_info         = $section['block'];
 		$block_info['mode'] = 'edit';
 
 		// Add the automatic render callback.
-		$block_info['render_callback'] = function( $block ) use( $section ) {
+		$block_info['render_callback'] = function ( $block ) use ( $section ) {
 			$inserting = core_default( 'inserting', $block['data'], false );
 
 			if ( $inserting ) {
 				if ( $section['includes_preview_image'] ) {
-					$preview_image_url = TPL_URL.'/packages/content/section-library/'.$section['location_rel'].'/preview.jpeg';
+					$preview_image_url = TPL_URL . '/packages/content/section-library/' . $section['location_rel'] . '/preview.jpeg';
 
-					Timber::render( CORE_PATH.'/layouts/block-preview.twig', [
-						'preview_image_url' => $preview_image_url
-					]);
+					Timber::render(
+						CORE_PATH . '/layouts/block-preview.twig',
+						array(
+							'preview_image_url' => $preview_image_url,
+						)
+					);
 				}
 			} else {
 				$block_fields = get_fields();
@@ -176,31 +197,33 @@ function core_register_sections_as_blocks() {
 
 		// Add preview data.
 		if ( $section['includes_preview_image'] ) {
-			$block_info['example'] = [
-				'attributes' => [
+			$block_info['example'] = array(
+				'attributes' => array(
 					'mode' => 'preview',
-					'data' => [ 'inserting' => true ]
-				]
-			];
+					'data' => array( 'inserting' => true ),
+				),
+			);
 		}
 
 		// Remove default supports.
-		$block_info['supports'] = [
-			'align' => false,
+		$block_info['supports'] = array(
+			'align'           => false,
 			'customClassName' => false,
-			'mode' => false,
-			'multiple' => $section['multiple']
-		];
+			'mode'            => false,
+			'multiple'        => $section['multiple'],
+		);
 
 		acf_register_block_type( $block_info );
 
 		// Register the Field Group.
-		core_register_field_group([
-			'title' => $section['title'],
-			'slug' => $section['slug'],
-			'location' => [ 'block' => "acf/{$section['slug']}" ],
-			'fields' => $section['fields']
-		]);
+		core_register_field_group(
+			array(
+				'title'    => $section['title'],
+				'slug'     => $section['slug'],
+				'location' => array( 'block' => "acf/{$section['slug']}" ),
+				'fields'   => $section['fields'],
+			)
+		);
 	}
 }
 
@@ -212,17 +235,22 @@ add_action( 'core_after_scan_sections', 'core_register_sections_as_blocks' );
  * @since 2.11.0
  * @internal
  *
- * @param int [$post_id=null]
+ * @param int [ $post_id=null]
  * @return mixed[]
  */
 function core_get_post_sections( $post_id = null ) {
-	if ( ! $post_id ) $post_id = get_the_ID();
+	if ( ! $post_id ) {
+		$post_id = get_the_ID();
+	}
 
 	$blocks = parse_blocks( get_post_field( 'post_content', $post_id ) );
 
-	return array_filter( $blocks, function( $block ) {
-		return $block['blockName'] !== null;
-	});
+	return array_filter(
+		$blocks,
+		function ( $block ) {
+			return $block['blockName'] !== null;
+		}
+	);
 }
 
 /**
@@ -232,21 +260,26 @@ function core_get_post_sections( $post_id = null ) {
  *
  * @since 1.0.0
  * @since 2.11.0 Lists sections added to the Block Editor,
- * 				 instead of Flexible content layouts.
+ *               instead of Flexible content layouts.
  * @internal
  *
- * @param int [$post_id=null]
+ * @param int [ $post_id=null]
  * @return string[]|null The list of sections present on the page.
  */
 function core_list_section_library_sections( $post_id = null ) {
-	if ( ! $post_id ) $post_id = get_the_ID();
+	if ( ! $post_id ) {
+		$post_id = get_the_ID();
+	}
 
 	$sections_list = core_get_post_sections( $post_id );
 
-	return empty( $sections_list ) ? [] : array_unique(
-		array_map( function( $section ) {
-			return str_replace( 'acf/', '', $section['blockName'] );
-		}, $sections_list )
+	return empty( $sections_list ) ? array() : array_unique(
+		array_map(
+			function ( $section ) {
+				return str_replace( 'acf/', '', $section['blockName'] );
+			},
+			$sections_list
+		)
 	);
 }
 
@@ -258,7 +291,9 @@ function core_list_section_library_sections( $post_id = null ) {
  */
 function core_check_section_library_support( $can_edit, $post_type ) {
 	$post_type = core_get_post_type( $post_type );
-	if ( ! $post_type ) return $can_edit;
+	if ( ! $post_type ) {
+		return $can_edit;
+	}
 
 	// $can_edit = core_content_type_supports( 'section-library', $post_type );
 

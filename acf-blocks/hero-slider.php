130ace50bfa6dc031f6c976d3d1c1374ee9d04f0
Sahil Mepani <productreseller@gmail.com>
2025-06-04 09:14:56 +0530

    Update

--- acf-blocks/hero-slider.php
+++ acf-blocks/hero-slider.php
@@ -8,7 +8,7 @@
 
 // Set thumbnail preview in backend.
 if ( isset( $block['data']['preview_image'] ) ) {
-	echo '<img src="' . esc_url( $block['data']['preview_image'] ) . '" style="inline-size:100%; block-size:auto;">';
+	echo '<img src="' . esc_url( $block['data']['preview_image'] ) . '" style="width:100%; height:auto;">';
 	return; // required.
 }
 
@@ -19,6 +19,11 @@ if ( 'on' !== $display ) {
 }
 
 // Data options.
+$slider = get_field( 'slider' );
+
+if ( ! is_array( $slider ) || empty( $slider ) ) {
+	return;
+}
 
 // Developer options.
 $spacing        = get_field( 'spacing' );
@@ -35,12 +40,64 @@ $spacing_bottom_custom = 'custom' === $spacing_bottom ? "--spacing-bottom-custom
 
 <section
 	class="hero-slider-section section <?php echo esc_attr( "section-display-{$display} {$spacing_top} {$spacing_bottom} {$custom_classes}" ); ?>"
-	style="<?php echo esc_attr( "{$spacing_top_custom} {$spacing_bottom_custom} {$custom_css}" ); ?>"
-	id="<?php echo esc_attr( $unique_id ); ?>">
+	style="<?php echo esc_attr( "{$spacing_top_custom} {$spacing_bottom_custom} {$custom_css}" ); ?> "
+	id="<?php echo esc_attr( $unique_id ); ?>" data-inview data-aos="fade">
 
-	<div class="container">
+	<div class="swiper hero-slider">
+		<!-- Additional required wrapper -->
+		<div class="swiper-wrapper">
+			<?php
+			$i = 1;
+			foreach ( $slider as $slide ) {
+				$desktop_image = $slide['desktop_image'] ?? DEFAULT_THUMBNAIL_ID;
+				$mobile_image  = $slide['mobile_image'] ?? '';
+				$mobile_class  = $mobile_image ? 'has-mobile' : '';
+				?>
+			<div class="swiper-slide slide">
+				<div class="img-cover-block">
+					<?php
+						$image_data = wp_get_attachment_image_src( $desktop_image, 'w1920' );
+						$image_alt  = get_post_meta( $desktop_image, '_wp_attachment_image_alt', true );
+						$image_alt  = trim( wp_strip_all_tags( $image_alt ) );
+					?>
+					<img src="<?php echo esc_attr( wp_get_attachment_image_url( $desktop_image, 'w1920' ) ); ?>"
+						srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( $desktop_image ) ); ?>"
+						sizes="100vw" alt="<?php echo esc_attr( $image_alt ); ?>"
+						width="<?php echo esc_attr( $image_data[1] ); ?>"
+						height="<?php echo esc_attr( $image_data[2] ); ?>"
+						class="img-cover img-desktop <?php echo esc_html( $mobile_class ); ?>"
+						<?php echo ( 0 !== $i ) ? 'loading="lazy"' : 'fetchpriority="high"'; ?> />
 
+					<?php
+					if ( $mobile_image ) {
+						$image_data = wp_get_attachment_image_src( $mobile_image, 'w1920' );
+						$image_alt  = get_post_meta( $mobile_image, '_wp_attachment_image_alt', true );
+						$image_alt  = trim( wp_strip_all_tags( $image_alt ) );
+						?>
+					<img src="<?php echo esc_attr( wp_get_attachment_image_url( $mobile_image, 'w1920' ) ); ?>"
+						srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( $mobile_image ) ); ?>"
+						sizes="100vw" alt="<?php echo esc_attr( $image_alt ); ?>"
+						width="<?php echo esc_attr( $image_data[1] ); ?>"
+						height="<?php echo esc_attr( $image_data[2] ); ?>" class="img-cover img-mobile"
+						<?php echo ( 0 !== $i ) ? 'loading="lazy"' : 'fetchpriority="high"'; ?> />
+					<?php } ?>
+				</div>
+			</div>
+			<?php } ?>
+		</div> <!-- .swiper-wrapper -->
 
+		<div class="swiper-pagination swiper-pagination-dot"></div>
 
-	</div><!-- .container -->
+		<?php
+		if ( count( $slider ) > 1 ) {
+			get_template_part(
+				'template-parts/swiper-navigation',
+				null,
+				array(
+					'style' => 'floating',
+				)
+			);
+		}
+		?>
+	</div> <!-- .swiper -->
 </section>
--- acf-blocks/logo-slider.php
+++ acf-blocks/logo-slider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Logo Slider ACF Block
+ * Logo Slider ACF block
  *
  * @package Skeleton
  * @subpackage ACF
@@ -8,7 +8,7 @@
 
 // Set thumbnail preview in backend.
 if ( isset( $block['data']['preview_image'] ) ) {
-	echo '<img src="' . esc_url( $block['data']['preview_image'] ) . '" style="inline-size:100%; block-size:auto;">';
+	echo '<img src="' . esc_url( $block['data']['preview_image'] ) . '" style="width:100%; height:auto;">';
 	return; // required.
 }
 
@@ -17,8 +17,12 @@ $display = get_field( 'display' );
 if ( 'on' !== $display ) {
 	return;
 }
-
 // Data options.
+$logos = get_field( 'logos' );
+
+if ( ! is_array( $logos ) || empty( $logos ) ) {
+	return;
+}
 
 // Developer options.
 $spacing        = get_field( 'spacing' );
@@ -40,7 +44,58 @@ $spacing_bottom_custom = 'custom' === $spacing_bottom ? "--spacing-bottom-custom
 
 	<div class="container">
 
+		<?php if ( is_array( $logos ) && ! empty( $logos ) ) { ?>
+		<div class="<?php echo skel_swiper_direction_class(); //phpcs:ignore ?> logo-slider" data-inview
+			data-aos="fade">
+			<div class="swiper-wrapper">
+				<?php
+				foreach ( $logos as $slide ) {
+					$logo = $slide['image'];
+					?>
+					<?php if ( $logo ) { ?>
+				<div class="swiper-slide slide">
+					<div class="logo-block">
+						<?php
+										$image_data = wp_get_attachment_image_src( $logo, 'w768' );
+										$image_alt  = get_post_meta( $logo, '_wp_attachment_image_alt', true );
+										$image_alt  = trim( wp_strip_all_tags( $image_alt ) );
+						?>
+						<img src="<?php echo esc_attr( wp_get_attachment_image_url( $logo, 'w768' ) ); ?>"
+							srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( $logo ) ); ?>" sizes="40rem"
+							alt="<?php echo esc_attr( $image_alt ); ?>"
+							width="<?php echo esc_attr( $image_data[1] ); ?>"
+							height="<?php echo esc_attr( $image_data[2] ); ?>" class="img-responsive" loading="lazy" />
+					</div>
+				</div> <!-- .swiper-slide -->
+				<?php } ?>
+				<?php } ?>
 
+				<?php
+				foreach ( $logos as $slide ) {
+					$logo = $slide['image'];
+					?>
+					<?php if ( $logo ) { ?>
+				<div class="swiper-slide slide">
+					<div class="logo-block">
+						<?php
+										$image_data = wp_get_attachment_image_src( $logo, 'w768' );
+										$image_alt  = get_post_meta( $logo, '_wp_attachment_image_alt', true );
+										$image_alt  = trim( wp_strip_all_tags( $image_alt ) );
+						?>
+						<img src="<?php echo esc_attr( wp_get_attachment_image_url( $logo, 'w768' ) ); ?>"
+							srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( $logo ) ); ?>" sizes="40rem"
+							alt="<?php echo esc_attr( $image_alt ); ?>"
+							width="<?php echo esc_attr( $image_data[1] ); ?>"
+							height="<?php echo esc_attr( $image_data[2] ); ?>" class="img-responsive" loading="lazy" />
+					</div>
+				</div> <!-- .swiper-slide -->
+				<?php } ?>
+				<?php } ?>
 
-	</div><!-- .container -->
+			</div> <!-- .swiper-wrapper -->
+		</div> <!-- .swiper -->
+		<?php } ?>
+
+	</div> <!-- .container -->
+
 </section>

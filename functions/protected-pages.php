fc1a0dba117a33a876cee6a74c02ddf6493d0dee
Sahil Mepani <productreseller@gmail.com>
2026-03-30 20:39:53 +0530

    fix: add PAGE_KB_ID to protected posts and streamline redirection logic in prevent_post_deletion function

--- functions/protected-pages.php
+++ functions/protected-pages.php
@@ -22,11 +22,11 @@ function prevent_post_deletion( $postid ) {
 	$protected_posts                                 = array();
 	defined( 'PAGE_404_ID' ) ? $protected_posts[]    = PAGE_404_ID : '';
 	defined( 'PAGE_SEARCH_ID' ) ? $protected_posts[] = PAGE_SEARCH_ID : '';
+	defined( 'PAGE_KB_ID' ) ? $protected_posts[]     = PAGE_KB_ID : '';
 
 	if ( in_array( $postid, $protected_posts, true ) ) {
 		// Redirect to the page list in the admin area with a protected_post flag.
-		$redirect_url = wp_nonce_url( admin_url( 'edit.php?post_type=page&protected_post=true' ), 'protected_post_notice' );
-		wp_safe_redirect( $redirect_url );
+		wp_safe_redirect( admin_url( 'edit.php?post_type=page&protected_post=true' ) );
 		exit;
 	}
 }
